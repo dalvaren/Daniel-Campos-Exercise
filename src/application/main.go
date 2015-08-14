c504a1c747130c0d5d2a305782aaca1299762f93
Daniel Campos <danielalvarengacampos@gmail.com>
2015-08-13 23:27:46 -0300

    add JWT auth

--- src/application/main.go
+++ src/application/main.go
@@ -11,56 +11,46 @@ package main
 // }
 
 import (
+	jwt_lib "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/contrib/jwt"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
-var DB = make(map[string]string)
+var (
+	tokenSecret = "weAreTheChampions"
+)
 
 func main() {
 	r := gin.Default()
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong2")
+	public := r.Group("/api")
+
+	public.GET("/", func(c *gin.Context) {
+		// Create the token
+		token := jwt_lib.New(jwt_lib.GetSigningMethod("HS256"))
+		// Set some claims
+		token.Claims["ID"] = "Christopher"
+		token.Claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+		// Sign and get the complete encoded token as a string
+		tokenString, err := token.SignedString([]byte(tokenSecret))
+		if err != nil {
+			c.JSON(500, gin.H{"message": "Could not generate token"})
+		}
+		c.JSON(200, gin.H{"token": tokenString})
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := DB[user]
-		if ok {
-			c.JSON(200, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(200, gin.H{"user": user, "status": "no value"})
-		}
+	private := r.Group("/api/private")
+	private.Use(jwt.Auth(tokenSecret))
+
+	/*
+		Set this header in your request to get here.
+		Authorization: Bearer `token`
+	*/
+
+	private.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello from private"})
 	})
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			DB[user] = json.Value
-			c.JSON(200, gin.H{"status": "ok"})
-		}
-	})
-
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	r.Run(":8081")
 }
